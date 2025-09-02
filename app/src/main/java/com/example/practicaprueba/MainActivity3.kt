ca30f712472e5e9fed1da6a06e653007d1bca6c1
FanDeChayanne <p.segovia@duocuc.cl>
2025-09-02 11:41:03 -0400

    se añade el convertor de pesos a usd

--- app/src/main/java/com/example/practicaprueba/MainActivity3.kt
+++ app/src/main/java/com/example/practicaprueba/MainActivity3.kt
@@ -1,10 +1,13 @@
 package com.example.practicaprueba
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practicaprueba.conversor.Conversor
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,21 @@ class MainActivity3 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+            val numeroConvertor:TextView = findViewById(R.id.txtConversor)
+            val botonConvertir:Button = findViewById(R.id.btnConversor)
+            val textoResultado:TextView = findViewById(R.id.txtTotal)
+
+            botonConvertir.setOnClickListener{
+
+                val numeroUno = numeroConvertor.text.toString().toIntOrNull() ?:0
+
+                var resultado = Conversor.convertir_usd_string(numeroUno)
+                textoResultado.text = resultado.toString()
+            }
+
+
+
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
